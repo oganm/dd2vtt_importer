087308354faea95a52ec1651d42d2cacba7a978c
OganM <ogan.mancarci@gmail.com>
2021-11-10 01:37:25 -0800

    object occluders and lighting

--- addons/dd2vtt/import_plugin.gd
+++ addons/dd2vtt/import_plugin.gd
@@ -31,11 +31,16 @@ func get_preset_name(preset):
 		_:
 			return "Unknown"
 
+var lights 
+
 func get_import_options(preset):
 	match preset:
 		Presets.DEFAULT:
 			return [{'name':'Walls as Occluders','default_value':true},
-					{'name':'Walls as Lines','default_value':true}]
+					{'name':'Walls as Lines','default_value':true},
+					{'name':'Load Lights','default_value':true},
+					{'name':"Light Texture",'default_value':load('res://addons/dd2vtt/light_textures/soft_texture.tres'),'property_hint':PROPERTY_HINT_RESOURCE_TYPE,'hint_string':'StreamTexture'},
+					{'name':"Occluder Objects",'default_value':true}]
 		_:
 			return []
 
--- addons/dd2vtt/util.gd
+++ addons/dd2vtt/util.gd
@@ -3,7 +3,10 @@ extends Reference
 # returns the scene after reading the dd2vtt output
 func parse_dd2vtt(data:Dictionary, options: Dictionary, root_name:String = 'Map')->Node2D:
 	print('parsing')
-	print(data.resolution)
+	print(data.keys())
+	print(data.objects_line_of_sight)
+	print(data.portals)
+	print(data.environment)
 	var root = Node2D.new()
 	root.name = root_name
 	
@@ -23,14 +26,23 @@ func parse_dd2vtt(data:Dictionary, options: Dictionary, root_name:String = 'Map'
 	
 	# read line of sight blockers
 	if options['Walls as Lines'] or options['Walls as Occluders']:
+		var LineWalls = Node2D.new()
+		var OccluderWalls = Node2D.new()
+		if options['Walls as Lines']:
+			root.add_child(LineWalls)
+			LineWalls.name = 'LineWalls'
+			LineWalls.owner = root
+		if options['Walls as Occluders']:
+			OccluderWalls.name = "OccluderWalls"
+			root.add_child(OccluderWalls)
+			OccluderWalls.owner = root
+		
 		for blocker in data.line_of_sight:
-			var offset = Vector2(-data.resolution.map_size.x*data.resolution.pixels_per_grid/2,
-								 -data.resolution.map_size.y*data.resolution.pixels_per_grid/2)
-			var points = dict2vector2array(blocker, data.resolution.pixels_per_grid,offset)
+			var points = dict2vector2array(blocker, data.resolution)
 			if options['Walls as Lines']:
 				var line:= Line2D.new()
 				line.points = points
-				root.add_child(line)
+				LineWalls.add_child(line)
 				line.owner = root
 			if options['Walls as Occluders']:
 				var occluder := LightOccluder2D.new()
@@ -40,8 +52,39 @@ func parse_dd2vtt(data:Dictionary, options: Dictionary, root_name:String = 'Map'
 				points.invert()
 				occluder_points.append_array(points)
 				occluder.occluder.polygon = occluder_points
-				root.add_child(occluder)
+				OccluderWalls.add_child(occluder)
 				occluder.owner = root
+				
+	if options['Load Lights']:
+		var Lights = Node2D.new()
+		root.add_child(Lights)
+		Lights.owner = root
+		Lights.name = 'Lights'
+		for light in data.lights:
+			var light2d := Light2D.new()
+			light2d.position = convert_coords(Vector2(light.position.x,light.position.y),data.resolution)
+			light2d.texture = StreamTexture.new()
+			light2d.set_texture(options['Light Texture'])
+			light2d.shadow_enabled = true
+			light2d.color = light.color
+			light2d.energy = light.intensity
+			var texture_size = light2d.texture.get_size().x
+			light2d.texture_scale = light.range*data.resolution.pixels_per_grid/texture_size*2
+			Lights.add_child(light2d)
+			light2d.owner = root
+			
+	if options['Occluder Objects']:
+		var Objects = Node2D.new()
+		root.add_child(Objects)
+		Objects.owner = root
+		Objects.name = 'Objects'
+		for object in data.objects_line_of_sight:
+			var points = dict2vector2array(object,data.resolution)
+			var occluder := LightOccluder2D.new()
+			occluder.occluder = OccluderPolygon2D.new()
+			occluder.occluder.polygon = points
+			Objects.add_child(occluder)
+			occluder.owner = root
 	return root
 
 # reads dd2vtt file to return a json
@@ -58,8 +101,13 @@ func read_json(file_path:String)->Dictionary:
 	return data_parse.result
 
 
-func dict2vector2array(dict_array:Array,multiplier:float, offset:Vector2):
+func convert_coords(vect: Vector2, resolution: Dictionary)->Vector2:
+	return Vector2(vect.x*resolution.pixels_per_grid -resolution.map_size.x*resolution.pixels_per_grid/2,
+				   vect.y*resolution.pixels_per_grid -resolution.map_size.x*resolution.pixels_per_grid/2)
+	
+
+func dict2vector2array(dict_array:Array,resolution:Dictionary):
 	var array: PoolVector2Array
 	for x in dict_array:
-		array.append(Vector2(x.x*multiplier+offset.x,x.y*multiplier+offset.y))
+		array.append(convert_coords(Vector2(x.x,x.y),resolution))
 	return array
